efaa10ff30eb632d414df4a0a228873077772a11
BadiatulRoziqin <badiatulroziqin82@gmail.com>
2025-04-24 22:59:55 +0700

    membuat project laravel dan install filament

---
+++ app/Filament/Resources/AlumniResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AlumniResource\Pages;
+use App\Filament\Resources\AlumniResource\RelationManagers;
+use App\Models\Alumni;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AlumniResource extends Resource
+{
+    protected static ?string $model = Alumni::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->required(),
+                Forms\Components\Select::make('jenis_kelamin')
+                    ->options([
+                        'Laki-laki' => 'Laki-laki',
+                        'Perempuan' => 'Perempuan',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('angkatan')
+                    ->required(),
+                Forms\Components\TextInput::make('pekerjaan'),
+                Forms\Components\TextInput::make('kontak'),
+                Forms\Components\Textarea::make('alamat')->rows(3),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')->searchable(),
+                Tables\Columns\TextColumn::make('jenis_kelamin'),
+                Tables\Columns\TextColumn::make('angkatan')->searchable(),
+                Tables\Columns\TextColumn::make('pekerjaan'),
+                Tables\Columns\TextColumn::make('kontak'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAlumnis::route('/'),
+            'create' => Pages\CreateAlumni::route('/create'),
+            'edit' => Pages\EditAlumni::route('/{record}/edit'),
+        ];
+    }
+}
